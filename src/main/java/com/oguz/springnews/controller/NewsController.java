cc2d143d94136fc8af0dd244697430c06b0a0564
oguzaktas <oguzaktass@gmail.com>
2019-05-02 10:12:58 +0300

    Update filtering News Categories

--- src/main/java/com/oguz/springnews/controller/NewsController.java
+++ src/main/java/com/oguz/springnews/controller/NewsController.java
@@ -5,7 +5,6 @@ import com.oguz.springnews.entity.News;
 import com.oguz.springnews.repository.NewsRepository;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
